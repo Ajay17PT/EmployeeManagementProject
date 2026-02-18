ac900106fc808f26bb73c1a1a49583cf9fcc66fe
Ajay17PT <kohinoorajaykumar@gmail.com>
2026-02-18 20:28:10 +0530

    Update database URL in Constants.java from localhost to mysql

--- src/main/java/com/utils/Constants.java
+++ src/main/java/com/utils/Constants.java
@@ -2,7 +2,7 @@ package com.utils;
 
 public class Constants {
 
-	public static final String URL = "jdbc:mysql://localhost:3306/Servlet_Jsp";
+	public static final String URL = "jdbc:mysql://mysql:3306/Servlet_Jsp";
 	
 	public static final String USERNAME = "root";
 	
